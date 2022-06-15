27fa377cd5b31285186b6f5609734b7e37c9b074
stardust6653 <stardust6653@gmail.com>
2022-06-16 01:52:08 +0900

    React 와 API를 활용한 영화앱 만들기
    API를 이용해 영화정보 불러오기 및 react-route-dom을 이용해 url 구성하기

---
+++ src/components/Detail_Movie.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+function Detail_Movie({id, coverImg, title, genres, rating, description}){
+  return (
+    <div>
+      <img src={coverImg} alt={title}/>
+      <h1>{title}</h1>
+      <div>
+        <span>평점 : {rating}</span><br/>
+        <span>제작연도 : {genres}</span>
+      </div>
+      <p>간편줄거리 : {description}</p>
+
+    </div>
+  )
+}
+
+Detail_Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  rating: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired
+}
+
+export default Detail_Movie;
---
+++ src/routes/Detail.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Detail_Movie from "../components/Detail_Movie";
+
+function Detail(){
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([])
+  const {id} = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json)
+    setMovies(json.data.movie);
+    setLoading(false)
+  }
+  useEffect(() => {
+    getMovie()
+  }, [])
+
+  return(
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Detail_Movie
+            key={movies.id}
+            id={movies.id}
+            coverImg={movies.medium_cover_image}
+            title={movies.title}
+            rating={movies.rating}
+            genres={movies.genres}
+            description={movies.description_full}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Detail;
